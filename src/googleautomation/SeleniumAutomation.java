22f77c179cd7daf293aa59731934347191ea0916
rashmiqatester <33202382+rashmiqatester@users.noreply.github.com>
2017-11-01 12:43:36 +0530

    my first automation project

---
+++ src/googleautomation/SeleniumAutomation.java
@@ -0,0 +1,20 @@
+package googleautomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumAutomation {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\IBM_ADMIN\\Desktop\\Selenium\\gecko1\\geckodriver.exe");
+		WebDriver driver =new FirefoxDriver();
+			driver.get("https://www.google.co.in/");
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			driver.quit();
+	}
+
+}
